77f883df3e96854c58177dd9faafcb0b2e982409
atis112 <toth.ati13@gmail.com>
2013-03-20 20:45:12 +0100

    Add audio

--- Android.mk
+++ Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(my-dir)
 
 ifeq ($(TARGET_DEVICE),protou) 
     subdir_makefiles := \
-#	 $(LOCAL_PATH)/libaudio/Android.mk
+	 $(LOCAL_PATH)/libaudio/Android.mk
      include $(subdir_makefiles)
 endif
